1eda65917b20411948b7a6a163f39a43f5d48679
Mateo <mateoibanhezsoruco@gmail.com>
2024-04-16 00:24:45 +0200

    Navbar 1

---
+++ bankam/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <div className='w-full min-h-screen bg-gray-950'>
+        <Header/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ bankam/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <>
+        <nav className="w-full h-auto bg-gray-800  lg:px-24 md:px-16 sm:px-14 px-12 py-2 shadow-md">
+            <div className='justify-between'>
+                {/*Logotipo cuando lo tengamos*/}
+                
+            </div>
+        </nav>
+        </>
+    )
+} 
+
+export default Header;
