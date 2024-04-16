335b13cf88490bfd5579edc943573c3f123dc5f6
Mateo <mateoibanhezsoruco@gmail.com>
2024-04-16 18:18:30 +0200

    Navbar fase 2

--- bankam/src/App.jsx
+++ bankam/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header'
+import { BrowserRouter as Router } from 'react-router-dom'; 
 
 function App() {
   return (
     <>
+    <Router>
       <div className='w-full min-h-screen bg-gray-950'>
         <Header/>
       </div>
+      </Router>
     </>
   );
 }
--- bankam/src/components/Header.jsx
+++ bankam/src/components/Header.jsx
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import logo from './logo.png'; // Importa el logotipo
 
 const Header = () => {
+    const [navbar, setNavbar] = useState(false);
+    const Navbar = [
+        {
+            name:'Inicio',
+            link:"/"
+        },
+        {
+            name:'Transferencias',
+            link:"/transferencias"
+        },
+        {
+            name:'Perfil',
+            link:"/perfil"
+        },
+        {
+            name:'Soporte',
+            link:"/soporte"
+        },
+        {
+            name:'Notificaciones',
+            link:"/notificaciones"
+        }
+    ];
 
     return (
-        <>
-        <nav className="w-full h-auto bg-gray-800  lg:px-24 md:px-16 sm:px-14 px-12 py-2 shadow-md">
-            <div className='justify-between'>
-                {/*Logotipo cuando lo tengamos*/}
-                
+        <nav className="w-full h-auto bg-gray-800 lg:px-24 md:px-16 sm:px-14 px-12 py-2 shadow-md">
+            <div className="justify-between mx-auto lg:w-full md:items-center md:flex">
+                {/* Zona de la navbar perteneciente al logo */}
+                <div>
+                    <div className="flex items-center justify-between py-3 md:py-5 md:block">
+                        {/* Logotipo */}
+                        <Link to="/" className="flex items-center">
+                            <img src={logo} alt="Logo" className="h-20 w-20 md:h-24 md:w-24" /> {/* Tamaño del logotipo */}
+                        </Link>
+                        {/* Toggle Button Section */}
+                        <div className="md:hidden">
+                            <button className="p-2 text-gray-700 rounded-md outline-none border border-transparent focus:border-gray-400 focus:border" onClick={() => setNavbar(!navbar)}>
+                                {navbar ? (
+                                    <FaTimes className="text-gray-400 cursor-pointer" size={24} />
+                                ) : (
+                                    <FaBars className="text-gray-400 cursor-pointer" size={24} />
+                                )}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* Items del Navbar */}
+                <div className={`flex justify-between items-center md:block ${navbar ? "block" : "hidden"}`}>
+                    <ul className="list-none lg:flex md:flex sm:block block gap-x-5 gap-y-16">
+                        {Navbar.map((item, index) => (
+                            <li key={index}>
+                                <Link to={item.link} className="text-gray-400 text-[1.15rem] font-medium tracking-wider hover:text-gray-200 ease-out duration-700">{item.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </nav>
-        </>
-    )
-} 
+    );
+};
 
 export default Header;
