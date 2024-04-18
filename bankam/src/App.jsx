57007a8224346976553a78329d8a4273f626ba6d
Mateo <mateoibanhezsoruco@gmail.com>
2024-04-18 19:04:09 +0200

    Vamos bien

--- bankam/src/App.jsx
+++ bankam/src/App.jsx
@@ -4,12 +4,13 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import HomeSection from './components/HomeSection';
 import Login from './components/Login';
 
-
 function App() {
   return (
     <>
     <Router>
+    <div class='text-white h-[100vh] flex-items-center justify-center bg-cover' style={{"backgroundImage": "url('../src/img/Login-bg.jpg')"}}>
       <Login/>
+      </div>
       <div className='w-full min-h-screen bg-gray-950'>
         <Header/>
         <HomeSection/>
--- bankam/src/components/Login.jsx
+++ bankam/src/components/Login.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
 
 const Login = () => {
-    
-    return(
-        <div>
-            <div className='bg-slate-800 border border-slate-600 rounded-md p-8 shadow-lg backdrop-fill'>
-                <h1 className="text-4xl font-bold text-center">Iniciar Sesion</h1>
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className='bg-slate-800 border border-slate-600 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-30 relative'>
+                <h1 className="text-4xl font-bold text-center mb-8">Iniciar Sesión</h1>
                 <form action="">
-                    <div>
-                        <input type='text'/>
-                        <label htmlFor=''>Nombre de Usuario</label>
+                    <div className='relative'>
+                        <input type='text' className='block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder=''/>
+                        <label htmlFor=''className='absolute text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown-scale-100 peer-focus:scale-75 peer-focus:-translate-y-6'>Nombre de Usuario</label>
                     </div>
-                    <div>
-                        <input type='password'/>
-                        <label htmlFor=''>Contrasena</label>
+                    <div className='relative my-4'>
+                        <input type='password' className='block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder=''/>
+                        <label htmlFor='' className='absolute text-sm duration-300 transform -translate scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark-text-blue-500 peer-placeholder-shown-scale-100 peer-focus:scale-75 peer-focus:-translate-y-6'>Contraseña</label>
                     </div>
+                    <button className="block w-full bg-blue-500 text-white py-2.5 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Iniciar Sesión</button>
                 </form>
             </div>
         </div>
-
     );
 };
 
-export default  Login;
+export default Login;
