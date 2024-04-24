27407159caeb97183e7ff561692ecfaf53c56cab
NoHayUnBeatMalo <aitorvazgar@gmail.com>
2024-04-24 17:27:13 +0200

    add bbdd

--- conector/bd_banco (1).sql
+++ conector/bd_banco (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-04-2024 a las 10:31:40
+-- Tiempo de generación: 24-04-2024 a las 17:26:43
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -193,254 +193,16 @@ INSERT INTO `tb_marcatarjeta` (`idMarca`, `nombre`) VALUES
 CREATE TABLE `tb_paises` (
   `idPais` int(11) NOT NULL,
   `iso` char(2) DEFAULT NULL,
-  `nombre` varchar(80) DEFAULT NULL
+  `nombre` varchar(80) DEFAULT NULL,
+  `codPais` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `tb_paises`
 --
 
-INSERT INTO `tb_paises` (`idPais`, `iso`, `nombre`) VALUES
-(1, 'AF', 'Afganistán'),
-(2, 'AX', 'Islas Gland'),
-(3, 'AL', 'Albania'),
-(4, 'DE', 'Alemania'),
-(5, 'AD', 'Andorra'),
-(6, 'AO', 'Angola'),
-(7, 'AI', 'Anguilla'),
-(8, 'AQ', 'Antártida'),
-(9, 'AG', 'Antigua y Barbuda'),
-(10, 'AN', 'Antillas Holandesas'),
-(11, 'SA', 'Arabia Saudí'),
-(12, 'DZ', 'Argelia'),
-(13, 'AR', 'Argentina'),
-(14, 'AM', 'Armenia'),
-(15, 'AW', 'Aruba'),
-(16, 'AU', 'Australia'),
-(17, 'AT', 'Austria'),
-(18, 'AZ', 'Azerbaiyán'),
-(19, 'BS', 'Bahamas'),
-(20, 'BH', 'Bahréin'),
-(21, 'BD', 'Bangladesh'),
-(22, 'BB', 'Barbados'),
-(23, 'BY', 'Bielorrusia'),
-(24, 'BE', 'Bélgica'),
-(25, 'BZ', 'Belice'),
-(26, 'BJ', 'Benin'),
-(27, 'BM', 'Bermudas'),
-(28, 'BT', 'Bhután'),
-(29, 'BO', 'Bolivia'),
-(30, 'BA', 'Bosnia y Herzegovina'),
-(31, 'BW', 'Botsuana'),
-(32, 'BV', 'Isla Bouvet'),
-(33, 'BR', 'Brasil'),
-(34, 'BN', 'Brunéi'),
-(35, 'BG', 'Bulgaria'),
-(36, 'BF', 'Burkina Faso'),
-(37, 'BI', 'Burundi'),
-(38, 'CV', 'Cabo Verde'),
-(39, 'KY', 'Islas Caimán'),
-(40, 'KH', 'Camboya'),
-(41, 'CM', 'Camerún'),
-(42, 'CA', 'Canadá'),
-(43, 'CF', 'República Centroafricana'),
-(44, 'TD', 'Chad'),
-(45, 'CZ', 'República Checa'),
-(46, 'CL', 'Chile'),
-(47, 'CN', 'China'),
-(48, 'CY', 'Chipre'),
-(49, 'CX', 'Isla de Navidad'),
-(50, 'VA', 'Ciudad del Vaticano'),
-(51, 'CC', 'Islas Cocos'),
-(52, 'CO', 'Colombia'),
-(53, 'KM', 'Comoras'),
-(54, 'CD', 'República Democrática del Congo'),
-(55, 'CG', 'Congo'),
-(56, 'CK', 'Islas Cook'),
-(57, 'KP', 'Corea del Norte'),
-(58, 'KR', 'Corea del Sur'),
-(59, 'CI', 'Costa de Marfil'),
-(60, 'CR', 'Costa Rica'),
-(61, 'HR', 'Croacia'),
-(62, 'CU', 'Cuba'),
-(63, 'DK', 'Dinamarca'),
-(64, 'DM', 'Dominica'),
-(65, 'DO', 'República Dominicana'),
-(66, 'EC', 'Ecuador'),
-(67, 'EG', 'Egipto'),
-(68, 'SV', 'El Salvador'),
-(69, 'AE', 'Emiratos Árabes Unidos'),
-(70, 'ER', 'Eritrea'),
-(71, 'SK', 'Eslovaquia'),
-(72, 'SI', 'Eslovenia'),
-(73, 'ES', 'España'),
-(74, 'UM', 'Islas ultramarinas de Estados Unidos'),
-(75, 'US', 'Estados Unidos'),
-(76, 'EE', 'Estonia'),
-(77, 'ET', 'Etiopía'),
-(78, 'FO', 'Islas Feroe'),
-(79, 'PH', 'Filipinas'),
-(80, 'FI', 'Finlandia'),
-(81, 'FJ', 'Fiyi'),
-(82, 'FR', 'Francia'),
-(83, 'GA', 'Gabón'),
-(84, 'GM', 'Gambia'),
-(85, 'GE', 'Georgia'),
-(86, 'GS', 'Islas Georgias del Sur y Sandwich del Sur'),
-(87, 'GH', 'Ghana'),
-(88, 'GI', 'Gibraltar'),
-(89, 'GD', 'Granada'),
-(90, 'GR', 'Grecia'),
-(91, 'GL', 'Groenlandia'),
-(92, 'GP', 'Guadalupe'),
-(93, 'GU', 'Guam'),
-(94, 'GT', 'Guatemala'),
-(95, 'GF', 'Guayana Francesa'),
-(96, 'GN', 'Guinea'),
-(97, 'GQ', 'Guinea Ecuatorial'),
-(98, 'GW', 'Guinea-Bissau'),
-(99, 'GY', 'Guyana'),
-(100, 'HT', 'Haití'),
-(101, 'HM', 'Islas Heard y McDonald'),
-(102, 'HN', 'Honduras'),
-(103, 'HK', 'Hong Kong'),
-(104, 'HU', 'Hungría'),
-(105, 'IN', 'India'),
-(106, 'ID', 'Indonesia'),
-(107, 'IR', 'Irán'),
-(108, 'IQ', 'Iraq'),
-(109, 'IE', 'Irlanda'),
-(110, 'IS', 'Islandia'),
-(111, 'IL', 'Israel'),
-(112, 'IT', 'Italia'),
-(113, 'JM', 'Jamaica'),
-(114, 'JP', 'Japón'),
-(115, 'JO', 'Jordania'),
-(116, 'KZ', 'Kazajstán'),
-(117, 'KE', 'Kenia'),
-(118, 'KG', 'Kirguistán'),
-(119, 'KI', 'Kiribati'),
-(120, 'KW', 'Kuwait'),
-(121, 'LA', 'Laos'),
-(122, 'LS', 'Lesotho'),
-(123, 'LV', 'Letonia'),
-(124, 'LB', 'Líbano'),
-(125, 'LR', 'Liberia'),
-(126, 'LY', 'Libia'),
-(127, 'LI', 'Liechtenstein'),
-(128, 'LT', 'Lituania'),
-(129, 'LU', 'Luxemburgo'),
-(130, 'MO', 'Macao'),
-(131, 'MK', 'ARY Macedonia'),
-(132, 'MG', 'Madagascar'),
-(133, 'MY', 'Malasia'),
-(134, 'MW', 'Malawi'),
-(135, 'MV', 'Maldivas'),
-(136, 'ML', 'Malí'),
-(137, 'MT', 'Malta'),
-(138, 'FK', 'Islas Malvinas'),
-(139, 'MP', 'Islas Marianas del Norte'),
-(140, 'MA', 'Marruecos'),
-(141, 'MH', 'Islas Marshall'),
-(142, 'MQ', 'Martinica'),
-(143, 'MU', 'Mauricio'),
-(144, 'MR', 'Mauritania'),
-(145, 'YT', 'Mayotte'),
-(146, 'MX', 'México'),
-(147, 'FM', 'Micronesia'),
-(148, 'MD', 'Moldavia'),
-(149, 'MC', 'Mónaco'),
-(150, 'MN', 'Mongolia'),
-(151, 'MS', 'Montserrat'),
-(152, 'MZ', 'Mozambique'),
-(153, 'MM', 'Myanmar'),
-(154, 'NA', 'Namibia'),
-(155, 'NR', 'Nauru'),
-(156, 'NP', 'Nepal'),
-(157, 'NI', 'Nicaragua'),
-(158, 'NE', 'Níger'),
-(159, 'NG', 'Nigeria'),
-(160, 'NU', 'Niue'),
-(161, 'NF', 'Isla Norfolk'),
-(162, 'NO', 'Noruega'),
-(163, 'NC', 'Nueva Caledonia'),
-(164, 'NZ', 'Nueva Zelanda'),
-(165, 'OM', 'Omán'),
-(166, 'NL', 'Países Bajos'),
-(167, 'PK', 'Pakistán'),
-(168, 'PW', 'Palau'),
-(169, 'PS', 'Palestina'),
-(170, 'PA', 'Panamá'),
-(171, 'PG', 'Papúa Nueva Guinea'),
-(172, 'PY', 'Paraguay'),
-(173, 'PE', 'Perú'),
-(174, 'PN', 'Islas Pitcairn'),
-(175, 'PF', 'Polinesia Francesa'),
-(176, 'PL', 'Polonia'),
-(177, 'PT', 'Portugal'),
-(178, 'PR', 'Puerto Rico'),
-(179, 'QA', 'Qatar'),
-(180, 'GB', 'Reino Unido'),
-(181, 'RE', 'Reunión'),
-(182, 'RW', 'Ruanda'),
-(183, 'RO', 'Rumania'),
-(184, 'RU', 'Rusia'),
-(185, 'EH', 'Sahara Occidental'),
-(186, 'SB', 'Islas Salomón'),
-(187, 'WS', 'Samoa'),
-(188, 'AS', 'Samoa Americana'),
-(189, 'KN', 'San Cristóbal y Nevis'),
-(190, 'SM', 'San Marino'),
-(191, 'PM', 'San Pedro y Miquelón'),
-(192, 'VC', 'San Vicente y las Granadinas'),
-(193, 'SH', 'Santa Helena'),
-(194, 'LC', 'Santa Lucía'),
-(195, 'ST', 'Santo Tomé y Príncipe'),
-(196, 'SN', 'Senegal'),
-(197, 'CS', 'Serbia y Montenegro'),
-(198, 'SC', 'Seychelles'),
-(199, 'SL', 'Sierra Leona'),
-(200, 'SG', 'Singapur'),
-(201, 'SY', 'Siria'),
-(202, 'SO', 'Somalia'),
-(203, 'LK', 'Sri Lanka'),
-(204, 'SZ', 'Suazilandia'),
-(205, 'ZA', 'Sudáfrica'),
-(206, 'SD', 'Sudán'),
-(207, 'SE', 'Suecia'),
-(208, 'CH', 'Suiza'),
-(209, 'SR', 'Surinam'),
-(210, 'SJ', 'Svalbard y Jan Mayen'),
-(211, 'TH', 'Tailandia'),
-(212, 'TW', 'Taiwán'),
-(213, 'TZ', 'Tanzania'),
-(214, 'TJ', 'Tayikistán'),
-(215, 'IO', 'Territorio Británico del Océano Índico'),
-(216, 'TF', 'Territorios Australes Franceses'),
-(217, 'TL', 'Timor Oriental'),
-(218, 'TG', 'Togo'),
-(219, 'TK', 'Tokelau'),
-(220, 'TO', 'Tonga'),
-(221, 'TT', 'Trinidad y Tobago'),
-(222, 'TN', 'Túnez'),
-(223, 'TC', 'Islas Turcas y Caicos'),
-(224, 'TM', 'Turkmenistán'),
-(225, 'TR', 'Turquía'),
-(226, 'TV', 'Tuvalu'),
-(227, 'UA', 'Ucrania'),
-(228, 'UG', 'Uganda'),
-(229, 'UY', 'Uruguay'),
-(230, 'UZ', 'Uzbekistán'),
-(231, 'VU', 'Vanuatu'),
-(232, 'VE', 'Venezuela'),
-(233, 'VN', 'Vietnam'),
-(234, 'VG', 'Islas Vírgenes Británicas'),
-(235, 'VI', 'Islas Vírgenes de los Estados Unidos'),
-(236, 'WF', 'Wallis y Futuna'),
-(237, 'YE', 'Yemen'),
-(238, 'DJ', 'Yibuti'),
-(239, 'ZM', 'Zambia'),
-(240, 'ZW', 'Zimbabue');
+INSERT INTO `tb_paises` (`idPais`, `iso`, `nombre`, `codPais`) VALUES
+(1, 'ES', 'España', 142800);
 
 -- --------------------------------------------------------
 
@@ -530,7 +292,9 @@ INSERT INTO `tb_tipoerror` (`idTipoError`, `nombre`) VALUES
 (2, 'Error de banco salida'),
 (3, 'Error interno'),
 (4, 'Error desconocido'),
-(5, 'Error sin fondos');
+(5, 'Error sin fondos'),
+(6, 'Sin error'),
+(7, 'Sin error');
 
 -- --------------------------------------------------------
 
@@ -622,25 +386,6 @@ INSERT INTO `tb_tipotarjeta` (`idTipoTarjeta`, `nombre`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `tb_tipotransaccion`
---
-
-CREATE TABLE `tb_tipotransaccion` (
-  `idTipo` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `tb_tipotransaccion`
---
-
-INSERT INTO `tb_tipotransaccion` (`idTipo`, `nombre`) VALUES
-(1, 'Salida'),
-(2, 'Entrada');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `tb_transaccion`
 --
 
@@ -649,7 +394,6 @@ CREATE TABLE `tb_transaccion` (
   `idClienteSalida` int(11) NOT NULL,
   `idBanco` int(11) NOT NULL,
   `cantidad` double(18,6) NOT NULL,
-  `idTipo` int(11) NOT NULL,
   `idEstado` int(11) NOT NULL,
   `idTipoError` int(11) DEFAULT NULL,
   `codigoSPEI` varchar(50) DEFAULT NULL
@@ -755,19 +499,12 @@ ALTER TABLE `tb_tipotarjeta`
   ADD PRIMARY KEY (`idTipoTarjeta`);
 
 --
--- Indices de la tabla `tb_tipotransaccion`
---
-ALTER TABLE `tb_tipotransaccion`
-  ADD PRIMARY KEY (`idTipo`);
-
---
 -- Indices de la tabla `tb_transaccion`
 --
 ALTER TABLE `tb_transaccion`
   ADD PRIMARY KEY (`idTransaccion`),
   ADD KEY `idTipoError` (`idTipoError`),
   ADD KEY `idEstado` (`idEstado`),
-  ADD KEY `idTipo` (`idTipo`),
   ADD KEY `idBanco` (`idBanco`),
   ADD KEY `idClienteSalida` (`idClienteSalida`);
 
@@ -821,7 +558,7 @@ ALTER TABLE `tb_marcatarjeta`
 -- AUTO_INCREMENT de la tabla `tb_paises`
 --
 ALTER TABLE `tb_paises`
-  MODIFY `idPais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=241;
+  MODIFY `idPais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=242;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_tarjeta`
@@ -845,7 +582,7 @@ ALTER TABLE `tb_tipoentidad`
 -- AUTO_INCREMENT de la tabla `tb_tipoerror`
 --
 ALTER TABLE `tb_tipoerror`
-  MODIFY `idTipoError` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idTipoError` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_tipomoneda`
@@ -858,12 +595,6 @@ ALTER TABLE `tb_tipomoneda`
 --
 ALTER TABLE `tb_tipotarjeta`
   MODIFY `idTipoTarjeta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT de la tabla `tb_tipotransaccion`
---
-ALTER TABLE `tb_tipotransaccion`
-  MODIFY `idTipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `tb_transaccion`
@@ -906,7 +637,6 @@ ALTER TABLE `tb_transaccion`
   ADD CONSTRAINT `tb_transaccion_ibfk_1` FOREIGN KEY (`idClienteSalida`) REFERENCES `tb_clientes` (`idCliente`),
   ADD CONSTRAINT `tb_transaccion_ibfk_2` FOREIGN KEY (`idBanco`) REFERENCES `tb_clientes` (`idCliente`),
   ADD CONSTRAINT `tb_transaccion_ibfk_3` FOREIGN KEY (`idEstado`) REFERENCES `tb_estadotransaccion` (`idEstado`),
-  ADD CONSTRAINT `tb_transaccion_ibfk_4` FOREIGN KEY (`idTipo`) REFERENCES `tb_tipotransaccion` (`idTipo`),
   ADD CONSTRAINT `tb_transaccion_ibfk_5` FOREIGN KEY (`idTipoError`) REFERENCES `tb_tipoerror` (`idTipoError`);
 COMMIT;
 
